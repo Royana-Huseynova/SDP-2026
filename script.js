af135cdd1727c4906e50af0c88d913f1937d09c3
UnifiedSatAI <sadatkhanov.huseyn9@gmail.com>
2026-03-13 12:23:26 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Satellite Benchmark loaded");
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+
+        if (targetSection) {
+            targetSection.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
